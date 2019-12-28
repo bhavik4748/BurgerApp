a509adf487c42d533d7afc368c3d7c6a6a84c8a0
Bhavik Gosrani <bhavik4748@gmail.com>
2019-12-28 14:54:31 -0500

    Added Order now button and logic for enable/disable for it

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,6 +22,8 @@ const buildControls = (props) => (
                 disabled={props.disabled[ctrl.type]}
             />
         ))}
+        <button disabled={!props.purchasable}
+            className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
