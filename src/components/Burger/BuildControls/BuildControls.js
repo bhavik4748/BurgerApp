a9f8f08378bfce235298d6eb2cdc84eb070f0446
Bhavik Gosrani <bhavik4748@gmail.com>
2019-12-28 19:07:22 -0500

    Added purchasehandler for showing Modal on Ordering

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,7 @@ const buildControls = (props) => (
                 disabled={props.disabled[ctrl.type]}
             />
         ))}
-        <button disabled={!props.purchasable}
+        <button onClick={props.ordered} disabled={!props.purchasable}
             className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,9 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
+
     }
 
     updatePurchaseState(ingredients) {
@@ -78,6 +80,9 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
 
     render() {
         const disabledInfo = { ...this.state.ingredients };
@@ -86,7 +91,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary
                         ingredients={this.state.ingredients}
                     ></OrderSummary>
@@ -98,6 +103,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
         );
