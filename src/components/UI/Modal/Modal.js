b2d9a511db0ee0d4cecce657b62fc0006ff6b32d
Bhavik Gosrani <bhavik4748@gmail.com>
2020-01-05 20:19:04 -0500

    Rearranging folders for better project structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxx';
+import Aux from '../../../hoc/Auxx/Auxx';
 import Backdrop from '../Backdrop/Backdrop';
 
 
