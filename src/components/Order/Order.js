050fb17ff236747d4a0d0d86c1f11c2669dafd35
Bhavik Gosrani <bhavik4748@gmail.com>
2020-02-22 18:53:31 -0500

    Added style and passing data to Order component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import classes from './Order.css';
 
-let Order = (props) => {
+let order = (props) => {
+    const ingredients = [];
+    for (let ingredient in props.ingredients) {
+        ingredients.push({
+            name: ingredient,
+            amount: props.ingredients[ingredient]
+        })
+    }
+    const ingredientOutput = ingredients.map(ig => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '0 5px'
+            }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad(1)</p>
-            <p>Price: <strong>USD 5.00</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
         </div>
     );
 }
 
-export default Order;
+export default order;
